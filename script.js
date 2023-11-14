484817a2bfc0d7004e146875434c0dc0b1d0ad0b
Guilhermezin07 <122484661+Guilhermezin07@users.noreply.github.com>
2023-11-14 18:43:17 -0300

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const container = document.querySelector(".flower-container");
-
-    function createFlower() {
-        const flower = document.createElement("div");
-        flower.classList.add("flower");
-
-        const petals = [];
-        for (let i = 0; i < 8; i++) {
-            const angle = (i * 45) * (Math.PI / 180);
-            const petal = document.createElement("div");
-            petal.classList.add("petal");
-            const petalX = Math.cos(angle) * 15;
-            const petalY = Math.sin(angle) * 15;
-            petal.style.left = `${petalX + 50}%`;
-            petal.style.top = `${petalY + 50}%`;
-            petals.push(petal);
-            flower.appendChild(petal);
-        }
-
-        const x = Math.random() * window.innerWidth;
-        const y = Math.random() * window.innerHeight;
-        flower.style.left = `${x}px`;
-        flower.style.top = `${y}px`;
-
-        container.appendChild(flower);
-
-        setTimeout(() => {
-            flower.style.display = "block";
-        }, 100);
-    }
-
-    setInterval(createFlower, 2000); // Cria uma flor a cada 2 segundos
+    const container = document.querySelector('.container');
+    const flower = document.createElement('div');
+    flower.classList.add('flower');
+    container.appendChild(flower);
 });
