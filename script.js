ac455719202e6a4cfcfa98f8a90329efcbc08b58
Guilhermezin07 <122484661+Guilhermezin07@users.noreply.github.com>
2023-11-14 17:53:18 -0300

    script.js

--- script.js
+++ script.js
@@ -8,25 +8,35 @@ document.addEventListener("DOMContentLoaded", function () {
         const stem = document.createElement("div");
         stem.classList.add("stem");
 
+        const center = document.createElement("div");
+        center.classList.add("flower-center");
+
         flower.appendChild(stem);
+        flower.appendChild(center);
         container.appendChild(flower);
 
         const petals = [];
-        for (let i = 0; i < 12; i++) {
+        for (let i = 0; i < 8; i++) {
+            const angle = (i * 45) * (Math.PI / 180);
             const petal = document.createElement("div");
             petal.classList.add("petal");
-            petal.style.transform = `rotate(${i * 30}deg) translate(0, -40px) rotate(0deg)`;
+            const petalX = Math.cos(angle) * 15;
+            const petalY = Math.sin(angle) * 15;
+            petal.style.left = `${petalX + 50}%`;
+            petal.style.top = `${petalY + 50}%`;
             petals.push(petal);
-            flower.appendChild(petal);
+            center.appendChild(petal);
         }
+
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
+        flower.style.left = `${x}px`;
+        flower.style.top = `${y}px`;
 
         setTimeout(() => {
             flower.style.display = "block";
-            petals.forEach(petal => {
-                petal.style.animation = "bloom 3s forwards";
-            });
         }, 100);
     }
 
-    setInterval(createFlower, 5000); // Cria uma flor a cada 5 segundos
+    setInterval(createFlower, 2000); // Cria uma flor a cada 2 segundos
 });
